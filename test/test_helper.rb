3b1e8eaa0d3e4c8d8b4da1dfb9d84aa956224490
Dan Kim <itsdanya@gmail.com>
2017-04-10 13:22:32 +0300

    Fix deprecation warnings in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,7 +18,7 @@ def setup_db
 end
 
 def teardown_db
-  ActiveRecord::Base.connection.tables.each do |table|
+  ActiveRecord::Base.connection.data_sources.each do |table|
     ActiveRecord::Base.connection.drop_table(table)
   end
 end
